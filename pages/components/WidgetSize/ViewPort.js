437eba2c3554661c4caa0ef376e9fdacd0b95f4a
evgenytryzo <g0022@inbox.ru>
2023-06-10 23:20:16 +0300

    Edd ViewPort

--- pages/components/WidgetSize/ViewPort.js
+++ pages/components/WidgetSize/ViewPort.js
@@ -1,20 +1,30 @@
 import styles from "./ViewPort.module.css"
 import { useState, useEffect } from "react"
 
-const ScreenWidth = () => {
+const ViewPort = () => {
   const [ width, setWidth ] = useState(0)
 
   const handleResize = () => {
     setWidth(window.innerWidth)
+    console.log("handleResize")
+  }
+
+  const onLoadHandler = () => {
+    window.addEventListener("resize", handleResize)
+  }
+
+  const onUnloadHandler = () => {
+    window.removeEventListener("resize", handleResize)
   }
 
   useEffect(() => {
-    handleResize()
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
+      handleResize()
+      onLoadHandler()
+      return onUnloadHandler
+    },
+    [])
 
   return <p className={ styles.ViewPort }>: { width } px</p>
 }
 
-export default ScreenWidth
+export default ViewPort
